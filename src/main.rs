329cc3463ed751d3a51b2b53182aadc8e8b3f50b
Alumin112 <alumin112.welcome@gmail.com>
2022-01-04 08:16:04 +0530

    challenge87

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+use std::io;
+
+mod challenge87;
+fn main() {
+    println!(":{}", 0 % 5);
+    for input in input_i32() {
+        println!("{}", challenge87::solve(input));
+    }
+}
+
+fn input_i32() -> Vec<i32> {
+    let mut input = String::new();
+
+    io::stdin().read_line(&mut input)
+        .expect("Failed to read line");
+    
+    let inputs: i32 = input
+        .trim()
+        .parse().unwrap();
+
+    return (0..inputs)
+        .map(|_| {
+            let mut input = String::new();
+
+            io::stdin().read_line(&mut input)
+                .expect("Failed to read line");
+
+            input.trim().parse::<i32>().unwrap()
+        })
+        .collect::<Vec<i32>>();
+}
