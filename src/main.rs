247c611b6f595fc0226f958d5dc67d6fb7620714
Alumin112 <alumin112.welcome@gmail.com>
2022-01-05 08:44:23 +0530

    last

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,27 @@
 use std::io;
 
-mod challenge87;
+mod challenge87 {
+    pub fn solve(k: u64) -> u64 {
+        for b in (2..k).filter(|b| k % b == 1) {
+            let mut j = k;
+            for i in 0..64 {
+                match j.checked_sub(b.pow(i)) {
+                    Some(j2) => {
+                        j = j2;
+                    }
+                    None => {
+                        break;
+                    }
+                }
+                if j == 0 {
+                    return b;
+                }
+            }
+        }
+        unreachable!();
+    }    
+}
+
 fn main() {
     for input in input_i32() {
         println!("{}", challenge87::solve(input));
